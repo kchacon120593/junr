5690697517b3ead7e208dcca593e1d9e1fd9bd3b
frans <FvD@users.noreply.github.com>
2016-03-17 15:02:26 -0600

    added get_guid and get_data

--- R/getdata.R
+++ R/getdata.R
@@ -2,11 +2,12 @@
 #'
 #' Get a list of all the available GUID's with datasets or views from the
 #' base URL
+#'
 #' @param base_url The base url of the Junar service
 #' @param api_token The user's API token for the Junar service
 #' @keywords GUID
 #' @export
-get_guid <- function(base_url, api_token){
+get_index <- function(base_url, api_token){
   if (missing(base_url)) {
     warning("Please add a valid base URL")
   }
@@ -15,9 +16,77 @@ get_guid <- function(base_url, api_token){
   }
   try({
     r <- GET(paste(base_url, "?auth_key=", api_token, sep=""), accept_json())
-     content_index <- fromJSON(content(r, "text"))
-     return(content_index$guid)
+    content_index <- fromJSON(content(r, "text"))
+    return(content_index)
   })
 }
 
+#' Show GUID list
+#'
+#' Show a simple list of all available GUID. This is not adding the titles to
+#' the list because it generaly will not fit in the console window.
+#'
+#' @param base_url The base url of the Junar service
+#' @param api_token The user's API token for the Junar service
+#' @keywords GUID
+#' @export
+guid_list <- function(base_url, api_token){
+  if (missing(base_url)) {
+    warning("Please add a valid base URL")
+  }
+  if (missing(api_token)) {
+    warning("Please add a valid api token for the base url you are trying to access")
+  }
+  try({
+    content_index <- get_index(base_url, api_token)
+    return(content_index$guid)
+  })
+}
+#' Show GUID titles
+#'
+#' Show a simple list of all available GUID titles. This is not adding the
+#' titles to the list because it generaly will not fit in the console window.
+#'
+#' @param base_url The base url of the Junar service
+#' @param api_token The user's API token for the Junar service
+#' @keywords GUID
+#' @export
+guid_titles <- function(base_url, api_token){
+  if (missing(base_url)) {
+    warning("Please add a valid base URL")
+  }
+  if (missing(api_token)) {
+    warning("Please add a valid api token for the base url you are trying to access")
+  }
+  try({
+    content_index <- get_index(base_url, api_token)
+    return(content_index$title)
+  })
+}
 
+#' Get data for a given GUID
+#' 
+#' Get the data for any given GUID
+#'
+#' @param base_url The base url of the Junar service
+#' @param api_token The user's API token for the Junar service
+#' @param guid The user's API token for the Junar service
+#' @keywords GUID
+#' @export
+
+get_data <- function(base_url, api_token, guid) {
+  if (missing(base_url)) {
+    warning("Please add a valid base URL")
+  }
+  if (missing(api_token)) {
+    warning("Please add a valid api token for the base url you are trying to access")
+  }
+  if (missing(guid)) {
+    warning("Please add a valid guid for the dataset you are trying to access")
+  }
+  try({
+    r <- GET(paste(base_url, guid, "/data.json/","?auth_key=", api_token, sep=""), accept_json())
+    dataset <- fromJSON(content(r, "text"))
+    return(dataset)
+  })
+}
